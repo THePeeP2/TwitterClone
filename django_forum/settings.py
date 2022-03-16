2555ff06a8d9711d48da20b10e9e72bc92701b30
Phillip Lewis Moffett II <99948287+THePeeP2@users.noreply.github.com>
2022-03-16 19:16:02 -0400

    Update settings.py

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-ilg4$pa*6nji_5mj4f#$n1)$#)6tq+i$mzqy_lt9u%cs7&)cmx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts'
+    'posts',
+    'cloudinary',
+   
 
 ]
 
